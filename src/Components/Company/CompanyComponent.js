f8abe9a2db748d8e112c440cee51ae39ee9e6fe0
klam41 <klam41@csc.com>
2019-01-23 16:44:06 +0700

    Company Page

--- src/Components/Company/CompanyComponent.js
+++ src/Components/Company/CompanyComponent.js
@@ -8,10 +8,19 @@ export default class CompanyComponent extends Component {
       <div className="CompanyComponent">
         <div className="d-flex flex-column flex-wrap main-content">
           <div className="company-bg"></div>
-          <div className="company-bg car-bg"></div>
+          <div className="company-bg car-bg d-flex justify-content-center">
+            <div className="d-flex flex-column justify-content-center">
+              <div className="d-flex justify-content-center">
+                <h1 className="text-color-orange text-center">Car Arrangement Service</h1>
+              </div>
+              <div className="d-flex justify-content-center">
+                <h1 className="text-color-orange text-center">(Rocket Platform)</h1>
+              </div>
+            </div>
+          </div>
           <div className="company-bg tech-bg d-flex justify-content-center">
             <div className="d-flex flex-column justify-content-center">
-              <h1 className="text-color-orange">
+              <h1 className="text-color-orange text-center">
                 Artificial Intelligence
                 </h1>
               <h1 className="text-color-orange text-center">
@@ -21,14 +30,42 @@ export default class CompanyComponent extends Component {
           </div>
           <div className="company-bg person-bg d-flex justify-content-center">
             <div className="d-flex flex-column justify-content-center">
-              <h1 className="text-color-orange">Software Outsourcing</h1>
+              <h1 className="text-color-orange text-center">Software Outsourcing</h1>
               <h1 className="text-color-orange text-center">(Korean & Vietnam)</h1>
             </div>
           </div>
-          <div className="company-bg car-road-bg"></div>
+          <div className="company-bg car-road-bg d-flex justify-content-center">
+            <div className="d-flex flex-column justify-content-center">
+              <div className="d-flex justify-content-center my-3">
+                <h1 className="text-color-orange txt-road-section text-center">Best Card Driving </h1>
+                <h1 className="text-road-orange text-center px-2">IT is</h1>
+                <h1 className="text-color-orange txt-road-section text-center">Intelligence Sincerity</h1>
+              </div>
+              <div className="d-flex justify-content-center my-3">
+                <h1 className="text-color-orange txt-road-section text-center">Ahead</h1>
+                <h1 className="text-road-orange text-center px-2"> A B C D S</h1>
+                <h1 className="text-color-orange txt-road-section text-center">Star, Service</h1>
+                <h1 className="text-color-orange txt-road-section text-center">Information Technology</h1>
+              </div>
+              <div className="d-flex flex-column justify-content-center my-auto short-description">
+                  <div className="row mx-auto">
+                    <div className="col-2 pr-0">
+                      <p className="text-center mb-0">In YWMobile ...</p>
+                    </div>
+                    <div className="col-6 pl-0 pr-5">
+                      <p className="text-center mb-0">there are people to make the world to go together</p>
+                    </div>
+                  </div>
+                  <p className="text-center mb-0">In YWMobile ...  there are people to make the world to go together</p>
+                  <p className="text-justify mb-0">We pursue happy & energic life with working</p>
+                  <p className="text-justify mb-0">We run sincerely for the goal to achieve</p>
+                  <p className="text-justify mb-0">Our mind is always opened for communication</p>
+              </div>
+            </div>
+          </div>
           <div className="company-bg d-flex justify-content-center">
           <div className="d-flex flex-column justify-content-center">
-              <img className="rounded mx-auto d-block sm-size" src={logo} alt="YWMobile Logo" />
+              <img className="rounded mx-auto d-block sm-size img-fluid" src={logo} alt="YWMobile Logo" />
               <div className="col text-center">
                 <p>
                   YWMobile CI keep the message about our will and endeavor for change the world to be more 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAngleUp, faIgloo } from '@fortawesome/free-solid-svg-icons'
-import { HashRouter } from 'react-router-dom'
+import {
+    library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+    faAngleUp,
+    faIgloo
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    HashRouter
+} from 'react-router-dom';
+import WebFont from 'webfontloader';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -20,11 +28,23 @@ require('../node_modules/letteringjs/jquery.lettering');
 
 library.add(faAngleUp, faIgloo);
 
-ReactDOM.render((<HashRouter>
-    <App />
-</HashRouter>), document.getElementById('root'));
+/* Load web font */
+WebFont.load({
+    google: {
+        families: ['Titillium Web:300,400,700', 'sans-serif',
+            'Shadows Into Light:300,400,700', 'cursive',
+            'ABeeZee', 'regular'
+        ]
+    }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(( < HashRouter >
+            <
+            App / >
+            <
+            /HashRouter>), document.getElementById('root'));
+
+            // If you want your app to work offline and load faster, you can change
+            // unregister() to register() below. Note this comes with some pitfalls.
+            // Learn more about service workers: http://bit.ly/CRA-PWA
+            serviceWorker.unregister();
